825d4f26309cd7fdf883d2ef64dec7889b7043ed
Outerleys <frederic.outerleys@sobeys.com>
2020-03-26 14:37:25 -0300

    just testing

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -14,7 +14,6 @@ namespace GitTest
         [STAThread]
         static void Main()
         {
-            //first changes
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
