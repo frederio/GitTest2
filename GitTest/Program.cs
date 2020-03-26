087ae946346f6eab2875ea16ec3bbf47a2f50ce2
Outerleys <frederic.outerleys@sobeys.com>
2020-03-26 13:50:28 -0300

    First change in Visual Studio

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -14,6 +14,7 @@ namespace GitTest
         [STAThread]
         static void Main()
         {
+            //first changes
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
